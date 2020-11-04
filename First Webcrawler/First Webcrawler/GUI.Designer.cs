7b8bca5c2999237f5aeca194414edc89d0e96632
IsaacFigNewton <48895941+IsaacFigNewton@users.noreply.github.com>
2020-11-03 21:52:39 -0800

    Can now get sites' HTML successfully (for the most part) and scan through for key phrases, albeit ineffectively
    The only times it can't get sites' HTML is when a connection is refused, aborted, or misdirected.
    It does look like it's scanning through the HTML, but it usually gives an out of bounds exception

--- First Webcrawler/First Webcrawler/GUI.Designer.cs
+++ First Webcrawler/First Webcrawler/GUI.Designer.cs
@@ -20,13 +20,14 @@ namespace First_Webcrawler
         public static int URLIndex = 1;
         public static string [] URLs = new String [NUMBER_OF_ENTRIES];
         public static string[] contactURLs = new String[NUMBER_OF_ENTRIES];
+        public static string[] knownContactURLs = {"https://sinwp.com/camera_clubs/"/*,  "https://sinwp.com/camera_clubs/"*/};
         //2-dimensional array of contact info in String form
         //ex: int[,] array2D = new int[,] { {email1, phone1, other1}, {email2, phone2, other2}, {email3, phone3, other3}};
         public static String[,] contactInfo = new String[NUMBER_OF_ENTRIES, NUMBER_OF_ENTRIES];
         public static String PATH_OF_IO_DOC = "C:\\Users\\Owner\\Desktop\\List of Camera Clubs.xlsx";
         public static String SHEET_NAME = "Midwest (ND,SD,NE,KS,OK,TX,MN,I";
 
-        public static String[] MAIN_PAGE_SEARCH_KEYWORDS = { "Contact", "Contact Us" };
+        public static String[] MAIN_PAGE_SEARCH_KEYWORDS = { "Contact", "Contact Us", "contact", "CONTACT"};
         public static String[,] CONTACTS_PAGE_SEARCH_KEYWORDS = { { "Email:", "Email-", "email:", "email-" }, { "Phone:", "Phone-", "phone:", "phone-" }, { "Other:", "Other-", "other:", "other-" } };
 
 
@@ -89,7 +90,17 @@ namespace First_Webcrawler
 
         private void buttonLocateContacts_Click(object sender, EventArgs e)
         {
-            try { 
+            while (URLIndex < URLs.Length)
+            {
+                lookAtHTML();
+            }
+        }
+
+        private static void lookAtHTML()
+        {
+            try
+            {                                                  //refactor to new method to run multiple times
+
                 while (URLIndex < URLs.Length)
                 {
                     string html;
@@ -98,22 +109,45 @@ namespace First_Webcrawler
                     //make sure the url is valid
                     if (!(url == null || url == ""))
                     {
-
-                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                        request.UserAgent = "C# console client";
-
-                        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-                        using (Stream stream = response.GetResponseStream())
-                        using (StreamReader reader = new StreamReader(stream))
+                        //if the url does not start with the url for the contacts page of a club or is too short to read (but not empty) then read the HTML
+                        if (((url.Length > 0) && (url.Length < knownContactURLs[0].Length)) || !((url.Substring(0, knownContactURLs[0].Length) == knownContactURLs[0]) /* || (url.Substring(0, knownContactURLs[1].Length) == knownContactURLs[1])*/))
                         {
-                            html = reader.ReadToEnd();
+                            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                            request.UserAgent = "C# console client";
+
+                            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                            using (Stream stream = response.GetResponseStream())
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                html = reader.ReadToEnd();
+                            }
+
+                            getContactURL(html);
+
+                            //print first 4 chars of HTML as indication of proper functioning
+                            Console.WriteLine(html.Substring(0, 15));
+                            Console.WriteLine(URLIndex);
+                            Console.WriteLine("");
                         }
+                        //otherwise if the url is a known contact URL, set the contactURLs entry to the respective url
+                        else if ((url.Substring(0, knownContactURLs[0].Length) == knownContactURLs[0])/* || (url.Substring(0, knownContactURLs[1].Length) == knownContactURLs[1])*/)
+                        {
+                            contactURLs[URLIndex] = url;
 
-                        getContactURL(html);
+                            Console.WriteLine(url.Substring(8, 22));
+                            Console.WriteLine(URLIndex);
+                            Console.WriteLine("");
 
-                        //Console.WriteLine(html);
-                        //Console.WriteLine("");
-                        Console.WriteLine(URLIndex);
+                        }
+                        //otherwise, print something else
+                        else if ((url.Length == 0)/* || (url.Substring(0, knownContactURLs[1].Length) == knownContactURLs[1])*/)
+                        {
+                            contactURLs[URLIndex] = url;
+
+                            Console.WriteLine("Empty URL");
+                            Console.WriteLine("");
+
+                        }
                     }
                     //increment the starting URLindex after an exception is seen so that it is incremented properly in the exception handlers
                     URLIndex++;
@@ -129,7 +163,8 @@ namespace First_Webcrawler
                 URLIndex++;
             }
             //catch the web exception and let user start again, starting at the next URL
-            catch (WebException ex) {
+            catch (WebException ex)
+            {
                 Console.WriteLine("Web (unable to resolve host name) Exception caught: {0}", ex, ", try again.");
                 getContactURL("");
                 URLIndex++;
@@ -138,11 +173,36 @@ namespace First_Webcrawler
 
         private static void getContactURL (string html)
         {
-            //make sure the input is not empty
-            if (html != "")
+            try
             {
-                //look through html for contacts page URL
+                //make sure the input is not empty
+                if (html != "")
+                {
+                    //buffer range included in limiting statement (refactorable but I'm too lazy and this should be good enough)
+                    for (int i = 0; i < html.Length - 20; i++)
+                    {
+                        //read through all of MAIN_PAGE_SEARCH_KEYWORDS
+                        for (int j = 0; j < MAIN_PAGE_SEARCH_KEYWORDS.Length; j++)
+                        {
+                            //if the site's HTML includes the keywords somewhere, look nearby it for the URL of the contacts page
+                            if (html.Substring(i, MAIN_PAGE_SEARCH_KEYWORDS[j].Length + i) == MAIN_PAGE_SEARCH_KEYWORDS[j])
+                            {
+                                //debugging
+                                Console.WriteLine("Found contacts phrase in HTML at character #" + i);
+                                //look through nearby html for contacts page URL
 
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Somehow there was no html at this URL");
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Looked for keyphrases outside of html for some reason.");
             }
         }
 
